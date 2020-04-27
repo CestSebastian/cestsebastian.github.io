ff4c032d4b101251f9baa41b5a15748f18180450
Sebastian Rus <ereses7@yahoo.com>
2020-04-27 22:48:46 +0300

    test

---
+++ games/sudoku-noir/precache-manifest.4e9856b4babff20c3c084a2cb882eca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2196e87d6e296f7735e4fe1413c252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcc80d7c5c1ba3dbbc9",
+    "url": "/static/css/main.61798504.chunk.css"
+  },
+  {
+    "revision": "858a90271501f4f7deac",
+    "url": "/static/js/2.a4549489.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4549489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abcc80d7c5c1ba3dbbc9",
+    "url": "/static/js/main.c0d596e5.chunk.js"
+  },
+  {
+    "revision": "21695cfe672e4865df76",
+    "url": "/static/js/runtime-main.bd284d52.js"
+  }
+]);
